abe5311c5fd491fa8f02a39b138a90c53ae18bfb
VietAnhh <viet.le175@gmail.com>
2016-11-21 06:41:53 -0500

    colors

--- app/pages/tasks/IconList/icon-list.ts
+++ app/pages/tasks/IconList/icon-list.ts
@@ -11,54 +11,54 @@ import {ViewController} from "ionic-angular/index";
       <button class="scan-button" ion-button icon-only (click)="GoBack()">Close</button>
     </ion-buttons>
   </ion-toolbar>
-</ion-header>
+</ion-header>csscsc
 
 <ion-content class="icon-list">
 <ion-row>
-    <ion-col><ion-icon name="ionic" color="primary"></ion-icon></ion-col>
-    <ion-col><ion-icon name="logo-angular"></ion-icon></ion-col>
-    <ion-col><ion-icon name="heart" color="danger"></ion-icon></ion-col>
-    <ion-col><ion-icon name="ionitron" color="primary"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="ionic" color="primary"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="logo-angular"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="heart" color="danger"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="ionitron" color="primary"></ion-icon></ion-col>
 
-    <ion-col><ion-icon name="happy" color="vibrant"></ion-icon></ion-col>
-    <ion-col><ion-icon name="people"></ion-icon></ion-col>
-    <ion-col><ion-icon name="person"></ion-icon></ion-col>
-    <ion-col><ion-icon name="contact"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="happy" color="vibrant"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="people"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="person"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="contact"></ion-icon></ion-col>
 
-    <ion-col><ion-icon name="apps"></ion-icon></ion-col>
-    <ion-col><ion-icon name="lock"></ion-icon></ion-col>
-    <ion-col><ion-icon name="key" color="bright"></ion-icon></ion-col>
-    <ion-col><ion-icon name="unlock"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="apps"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="lock"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="key" color="bright"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="unlock"></ion-icon></ion-col>
 
-    <ion-col><ion-icon name="map" color="secondary"></ion-icon></ion-col>
-    <ion-col><ion-icon name="navigate"></ion-icon></ion-col>
-    <ion-col><ion-icon name="pin"></ion-icon></ion-col>
-    <ion-col><ion-icon name="locate"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="map" color="secondary"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="navigate"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="pin"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="locate"></ion-icon></ion-col>
 
-    <ion-col><ion-icon name="mic"></ion-icon></ion-col>
-    <ion-col><ion-icon name="musical-notes" color="vibrant"></ion-icon></ion-col>
-    <ion-col><ion-icon name="volume-up"></ion-icon></ion-col>
-    <ion-col><ion-icon name="microphone"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="mic"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="musical-notes" color="vibrant"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="volume-up"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="microphone"></ion-icon></ion-col>
 
-    <ion-col><ion-icon name="cafe" color="bright"></ion-icon></ion-col>
-    <ion-col><ion-icon name="calculator"></ion-icon></ion-col>
-    <ion-col><ion-icon name="bus"></ion-icon></ion-col>
-    <ion-col><ion-icon name="wine" color="danger"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="cafe" color="bright"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="calculator"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="bus"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="wine" color="danger"></ion-icon></ion-col>
 
-    <ion-col><ion-icon name="camera"></ion-icon></ion-col>
-    <ion-col><ion-icon name="image" color="secondary"></ion-icon></ion-col>
-    <ion-col><ion-icon name="star" color="bright"></ion-icon></ion-col>
-    <ion-col><ion-icon name="pin"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="camera"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="image" color="secondary"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="star" color="bright"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="pin"></ion-icon></ion-col>
 
-    <ion-col><ion-icon name="arrow-dropup-circle" color="vibrant"></ion-icon></ion-col>
-    <ion-col><ion-icon name="arrow-back"></ion-icon></ion-col>
-    <ion-col><ion-icon name="arrow-dropdown"></ion-icon></ion-col>
-    <ion-col><ion-icon name="arrow-forward"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="arrow-dropup-circle" color="vibrant"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="arrow-back"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="arrow-dropdown"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="arrow-forward"></ion-icon></ion-col>
 
-    <ion-col><ion-icon name="cloud"></ion-icon></ion-col>
-    <ion-col><ion-icon name="sunny" color="bright"></ion-icon></ion-col>
-    <ion-col><ion-icon name="umbrella"></ion-icon></ion-col>
-    <ion-col><ion-icon name="rainy" color="primary"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="cloud"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="sunny" color="bright"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="umbrella"></ion-icon></ion-col>
+    <ion-col><ion-icon class="icon-array-list" name="rainy" color="primary"></ion-icon></ion-col>
   </ion-row>
 </ion-content>
 
@@ -67,11 +67,49 @@ import {ViewController} from "ionic-angular/index";
 `
 })
 export class IconList{
+
+    prettyColors: any[] = ['#E4004F','#F39801','#019E97','#00A0E8'];
+
     constructor(public vc: ViewController) { }
 
+
+  ionViewDidEnter(){
+    var x = document.getElementsByClassName("icon-array-list");
+    console.log(x);
+
+    // console.log(x[0].style.color )
+
+    console.log(x[0].style);
+    // for(let i = 0; i < x.length; i++){
+    //   x[i].style.color = `this.prettyColors[2]`;
+    // }
+
+
+
+  }
 
   GoBack(){
     this.vc.dismiss();
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
