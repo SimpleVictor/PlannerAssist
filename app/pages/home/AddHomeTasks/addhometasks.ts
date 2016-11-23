d8ff52c694cbe158de63835a16971c4c786781b3
VietAnhh <viet.le175@gmail.com>
2016-11-23 05:34:02 -0500

    small snimation fix

--- app/pages/home/AddHomeTasks/addhometasks.ts
+++ app/pages/home/AddHomeTasks/addhometasks.ts
@@ -1,6 +1,9 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, NgZone} from '@angular/core';
 import {NavController, ViewController, ModalController, NavParams} from 'ionic-angular';
 import {TaskList} from "../TaskList/tasklist";
+
+declare var TweenLite;
+declare var Circ;
 
 @Component({
   templateUrl: 'build/pages/home/AddHomeTasks/addhometasks.html',
@@ -17,10 +20,15 @@ export class AddHomeTask {
 
   myCurrentTask;
 
+  takeOutFirstInput: boolean = true;
+
+  zone;
+
 
   constructor(public navCtrl: NavController, public vc: ViewController, public modalCtrl: ModalController, public navParams: NavParams) {
 
     this.myTasks = this.navParams.get("AllTask");
+    this.zone = new NgZone({enableLongStackTrace: false});
 
   }
 
@@ -52,9 +60,24 @@ export class AddHomeTask {
     let modal = this.modalCtrl.create(TaskList, {AllTask: this.myTasks});
     modal.onDidDismiss(data => {
       if(data){
+
         console.log(data);
-        this.myCurrentTask = this.myTasks[data.index];
-        console.log(this.myCurrentTask);
+        this.zone.run(() => {
+          this.takeOutFirstInput = false;
+          this.myCurrentTask = this.myTasks[data.index];
+          console.log(this.myCurrentTask);
+
+          setTimeout(() => {
+            let spinMe = document.getElementById("spinHimIn");
+            spinMe.style.opacity = "1";
+            TweenLite.from(spinMe, 0.3, {left:"-100%", ease:Circ.easeOut});
+          }, 100);
+
+
+
+        });
+
+
       }
     });
     modal.present();
